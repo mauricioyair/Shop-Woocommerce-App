f2752dd388421ee537e3c4fc9dc43888affb4526
Mauricio Yair <yairchavez555@gmail.com>
2023-06-08 09:36:02 -0600

    [ADD] Components, Config Fetch by products

---
+++ src/context/GlobalState.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+import Context from "./Context";
+import config from "../resources/config";
+import apiFetch from "../resources/apiFetch";
+
+const globalState = props => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const newProducts = await apiFetch.get(
+      `${config.siteUrl}products?${config.wcCredentials}`
+    )
+    console.log(newProducts);
+    setProducts(newProducts);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <Context.Provider value={{
+      products,
+    }}>
+      {props.children}
+    </Context.Provider>
+  )
+};
+
+export default globalState;
