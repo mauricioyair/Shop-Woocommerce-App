0fe0dc8ad7863e4905786c77bd04a7ecf627c5cb
Mauricio Yair <yairchavez555@gmail.com>
2023-12-16 18:42:56 -0600

    Update header style and color in ProductsStack

--- src/stacks/ProductsStack.js
+++ src/stacks/ProductsStack.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 
 const ProductsStack = () => {
   return(
-    <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Navigator screenOptions={{ headerTintColor: 'white',headerStyle: { backgroundColor: 'black'} }}>
       <Stack.Screen name="Products" component={ProductsScreen} />
       <Stack.Screen name="Products Details" component={ProductDetailsScreen} />
       <Stack.Screen name="Cart" component={CartScreen} />
