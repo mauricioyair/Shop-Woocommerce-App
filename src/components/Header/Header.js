963705785b47983291e11db4649389a0b48c990b
Mauricio Yair <yairchavez555@gmail.com>
2023-12-16 18:43:31 -0600

    Update Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { View, Image, StyleSheet, Pressable } from "react-native";
+import { View, Image, StyleSheet, Pressable, Text } from "react-native";
 
 import {sizes} from '../../resources/constants';
-import logo from "../../assets/images/header/logo.png";
 import cartBtnIcon from '../../assets/images/header/cart-icon.png';
 import goBackBtn from "../../assets/images/header/go-back-icon.png";
 
@@ -16,8 +15,8 @@ const Header = props => {
         <Pressable onPress={() => navigation.goBack()}>
           <Image style={styles.arrowIcon} source={goBackBtn} />
         </Pressable>
-      )}
-      <Image style={styles.logo} source={logo} />
+      )}      
+      <Text style={styles.logo}>Shop-Woocomerce</Text>
       <Pressable onPress={() => navigation.navigate('Cart')}>
         <Image style={styles.cartBtn} source={cartBtnIcon} />
       </Pressable>
@@ -36,15 +35,15 @@ const styles = StyleSheet.create({
   arrowIcon: {
     width: 25,
     height: 21,
-  },
-  logo: {
-    width: 96,
-    height: 17,
-  },
+  }, 
   cartBtn: {
     width: 25,
     height: 25,
   },
+  logo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
 
 export default Header;
